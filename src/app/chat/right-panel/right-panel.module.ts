8b82ad207797095f8471a27e0b9bff2effc5adee
Mayank Jain <mayankjain1394@gmail.com>
2020-09-12 15:43:55 +0530

    Initial commit

---
+++ src/app/chat/right-panel/right-panel.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RightPanelComponent } from './right-panel.component';
+
+
+
+@NgModule({
+  declarations: [RightPanelComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class RightPanelModule { }
