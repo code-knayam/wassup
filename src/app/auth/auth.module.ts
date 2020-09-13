5d3056da7e196eb2f532b3d599d6b5a075d4bfb1
Mayank Jain <mayankjain1394@gmail.com>
2020-09-13 21:55:47 +0530

    Signin/up basic form

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -5,8 +5,17 @@ import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
 import { AuthRoutingModule } from './auth-routing.module';
 import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
 @NgModule({
   declarations: [AuthComponent, SigninComponent, SignupComponent],
-  imports: [CommonModule, AuthRoutingModule, MatCardModule],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
 })
 export class AuthModule {}
